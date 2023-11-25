a9025a118ca8f281d0198144af12abe9c216bd9e
Dafanurfiat <nurfiatdaffa@gmail.com>
2023-11-26 01:41:56 +0800

    Pengubahan Login

--- pages/samples/login.php
+++ pages/samples/login.php
@@ -34,15 +34,6 @@ if (isset($_POST['submit'])) {
 }
 ?>
 
-
-
-
-
-
-
-
-
-
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -62,6 +53,11 @@ if (isset($_POST['submit'])) {
     <link rel="stylesheet" href="../../assets/css/style.css">
     <!-- End layout styles -->
     <link rel="shortcut icon" href="../../assets/images/favicon.png" />
+    <style>
+        .enter-btn {
+            width: 200px; /* Adjust the width to your preference */
+        }
+    </style>
   </head>
   <body>
     <div class="container-scroller">
@@ -74,29 +70,15 @@ if (isset($_POST['submit'])) {
                 <form action="" method="POST">
                   <div class="form-group">
                     <label>Username or email *</label>
-                    <input type="email" name="email" class="form-control p_input">
+                    <input type="email" name="email" class="form-control p_input" placeholder="Enter your username">
                   </div>
                   <div class="form-group">
                     <label>Password *</label>
-                    <input type="password" name="password" class="form-control p_input">
-                  </div>
-                  <div class="form-group d-flex align-items-center justify-content-between">
-                    <div class="form-check">
-                      <label class="form-check-label">
-                        <input type="checkbox" class="form-check-input"> Remember me </label>
-                    </div>
-                    <a href="#" class="forgot-pass">Forgot password</a>
+                    <input type="password" name="password" class="form-control p_input" placeholder="Enter your password">
                   </div>
                   <div class="text-center">
-                    <button type="submit" name="submit" class="btn btn-primary btn-block enter-btn">Login</button>
+                    <button type="submit" name="submit" class="btn btn-outline-primary btn-block enter-btn">Login</button>
                   </div>
-                  <div class="d-flex">
-                    <button class="btn btn-facebook me-2 col">
-                      <i class="mdi mdi-facebook"></i> Facebook </button>
-                    <button class="btn btn-google col">
-                      <i class="mdi mdi-google-plus"></i> Google plus </button>
-                  </div>
-                  <p class="sign-up">Don't have an Account?<a href="#"> Sign Up</a></p>
                 </form>
               </div>
             </div>
