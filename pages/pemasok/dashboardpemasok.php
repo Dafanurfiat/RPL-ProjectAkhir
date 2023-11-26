8a277eceafc4521d1b923ffd0242218d4ade6e9c
Dafanurfiat <nurfiatdaffa@gmail.com>
2023-11-26 20:32:21 +0800

    rename&pindah location file

--- pages/pemasok/dashboardpemasok.php
+++ pages/pemasok/dashboardpemasok.php
@@ -1,10 +1,10 @@
 <?php
-include 'config.php';
+include '../../config.php';
 session_start();
 
  
 if (!isset($_SESSION['username'])) {
-    header("Location: indexpemasok.php");
+    header("Location: ../../login.php");
     exit(); // Terminate script execution after the redirect
 }
 ?>
@@ -17,36 +17,36 @@ if (!isset($_SESSION['username'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Tenda Biru</title>
     <!-- plugins:css -->
-    <link rel="stylesheet" href="assets/vendors/mdi/css/materialdesignicons.min.css">
-    <link rel="stylesheet" href="assets/vendors/css/vendor.bundle.base.css">
+    <link rel="stylesheet" href="../../assets/vendors/mdi/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="../../assets/vendors/css/vendor.bundle.base.css">
     <!-- endinject -->
     <!-- Plugin css for this page -->
-    <link rel="stylesheet" href="assets/vendors/jvectormap/jquery-jvectormap.css">
-    <link rel="stylesheet" href="assets/vendors/flag-icon-css/css/flag-icon.min.css">
-    <link rel="stylesheet" href="assets/vendors/owl-carousel-2/owl.carousel.min.css">
-    <link rel="stylesheet" href="assets/vendors/owl-carousel-2/owl.theme.default.min.css">
+    <link rel="stylesheet" href="../../assets/vendors/jvectormap/jquery-jvectormap.css">
+    <link rel="stylesheet" href="../../assets/vendors/flag-icon-css/css/flag-icon.min.css">
+    <link rel="stylesheet" href="../../assets/vendors/owl-carousel-2/owl.carousel.min.css">
+    <link rel="stylesheet" href="../../assets/vendors/owl-carousel-2/owl.theme.default.min.css">
     <!-- End plugin css for this page -->
     <!-- inject:css -->
     <!-- endinject -->
     <!-- Layout styles -->
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="../../assets/css/style.css">
     <!-- End layout styles -->
-    <link rel="shortcut icon" href="assets/images/TendaBiru.png" />
+    <link rel="shortcut icon" href="../../assets/images/TendaBiru.png" />
   </head>
   <body>
     <div class="container-scroller">
       <!-- partial:partials/_sidebar.html -->
       <nav class="sidebar sidebar-offcanvas" id="sidebar">
         <div class="sidebar-brand-wrapper d-none d-lg-flex align-items-center justify-content-center fixed-top">
-          <a class="sidebar-brand brand-logo" href="../../indexpemasok.php" style="color: #659DBD;"><h1>Tenda Biru</h1></a>
-          <a class="sidebar-brand brand-logo-mini" href="indexpemasok.php"><img src="assets/images/TendaBiru.png" alt="logo" /></a>
+          <a class="sidebar-brand brand-logo" href="dashboardpemasok.php" style="color: #659DBD;"><h1>Tenda Biru</h1></a>
+          <a class="sidebar-brand brand-logo-mini" href="dashboardpemasok.php"><img src="../../assets/images/TendaBiru.png" alt="logo" /></a>
         </div>
         <ul class="nav">
           <li class="nav-item profile">
             <div class="profile-desc">
               <div class="profile-pic">
                 <div class="count-indicator">
-                  <img class="img-xs rounded-circle " src="assets/images/faces/face15.jpg" alt="">
+                  <img class="img-xs rounded-circle " src="../../assets/images/faces/face15.jpg" alt="">
                   <span class="count bg-success"></span>
                 </div>
                 <div class="profile-name">
@@ -59,7 +59,7 @@ if (!isset($_SESSION['username'])) {
             <span class="nav-link">Navigation</span>
           </li>
           <li class="nav-item menu-items">
-            <a class="nav-link" href="indexpemasok.php">
+            <a class="nav-link" href="dashboardpemasok.php">
               <span class="menu-icon">
                 <i class="mdi mdi-home-variant"></i>
               </span>
@@ -67,7 +67,7 @@ if (!isset($_SESSION['username'])) {
             </a>
           </li>
           <li class="nav-item menu-items">
-            <a class="nav-link" href="pages/pemasok/transactionpemasok.php">
+            <a class="nav-link" href="transactionpemasok.php">
               <span class="menu-icon">
                 <i class="mdi mdi-file-plus"></i>
               </span>
@@ -75,7 +75,7 @@ if (!isset($_SESSION['username'])) {
             </a>
           </li>
           <li class="nav-item menu-items">
-            <a class="nav-link" href="pages/pemasok/deliverypemasok.php">
+            <a class="nav-link" href="deliverypemasok.php">
               <span class="menu-icon">
                 <i class="mdi mdi-truck"></i>
               </span>
@@ -89,7 +89,7 @@ if (!isset($_SESSION['username'])) {
         <!-- partial:partials/_navbar.html -->
         <nav class="navbar p-0 fixed-top d-flex flex-row">
           <div class="navbar-brand-wrapper d-flex d-lg-none align-items-center justify-content-center">
-            <a class="navbar-brand brand-logo-mini" href="indexpemasok.php"><img src="assets/images/logo-mini.svg" alt="logo" /></a>
+            <a class="navbar-brand brand-logo-mini" href="indexpemasok.php"><img src="../../assets/images/logo-mini.svg" alt="logo" /></a>
           </div>
           <div class="navbar-menu-wrapper flex-grow d-flex align-items-stretch">
             <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
@@ -153,7 +153,7 @@ if (!isset($_SESSION['username'])) {
               <li class="nav-item dropdown">
                 <a class="nav-link" id="profileDropdown" href="#" data-bs-toggle="dropdown">
                   <div class="navbar-profile">
-                    <img class="img-xs rounded-circle" src="assets/images/faces/face15.jpg" alt="">
+                    <img class="img-xs rounded-circle" src="../../assets/images/faces/face15.jpg" alt="">
                     <p class="mb-0 d-none d-sm-block navbar-profile-name"><?php echo $_SESSION['username']; ?></p>
                     <i class="mdi mdi-menu-down d-none d-sm-block"></i>
                   </div>
@@ -204,7 +204,7 @@ if (!isset($_SESSION['username'])) {
                             <td> 1 </td>
                             <td> May 15, 2015 </td>
                             <td>
-                            <a class="nav-link" href="pages/pemasok/transactiondetail.php">
+                            <a class="nav-link" href="transactiondetail.php">
                                 <button type="button" class="btn btn-outline-primary btn-icon-text"> Detail </button>  
                               </a> 
                             </td>
@@ -214,7 +214,7 @@ if (!isset($_SESSION['username'])) {
                             <td> 2 </td>
                             <td> July 1, 2015 </td>
                             <td>
-                            <a class="nav-link" href="pages/pemasok/transactiondetail.php">
+                            <a class="nav-link" href="transactiondetail.php">
                                 <button type="button" class="btn btn-outline-primary btn-icon-text"> Detail </button>  
                               </a> 
                             </td>
@@ -244,7 +244,7 @@ if (!isset($_SESSION['username'])) {
                             <td> 1 </td>
                             <td> May 15, 2015 </td>
                             <td>
-                            <a class="nav-link" href="pages/pemasok/detaildelivery.php">
+                            <a class="nav-link" href="detaildelivery.php">
                                 <button type="button" class="btn btn-outline-primary btn-icon-text"> Detail </button>  
                               </a> 
                             </td>
@@ -253,7 +253,7 @@ if (!isset($_SESSION['username'])) {
                             <td> 2 </td>
                             <td> July 1, 2015 </td>
                             <td>
-                            <a class="nav-link" href="pages/pemasok/detaildelivery.php">
+                            <a class="nav-link" href="detaildelivery.php">
                                 <button type="button" class="btn btn-outline-primary btn-icon-text"> Detail </button>  
                               </a> 
                             </td>
@@ -280,25 +280,25 @@ if (!isset($_SESSION['username'])) {
     </div>
     <!-- container-scroller -->
     <!-- plugins:js -->
-    <script src="assets/vendors/js/vendor.bundle.base.js"></script>
+    <script src="../../assets/vendors/js/vendor.bundle.base.js"></script>
     <!-- endinject -->
     <!-- Plugin js for this page -->
-    <script src="assets/vendors/chart.js/Chart.min.js"></script>
-    <script src="assets/vendors/progressbar.js/progressbar.min.js"></script>
-    <script src="assets/vendors/jvectormap/jquery-jvectormap.min.js"></script>
-    <script src="assets/vendors/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
-    <script src="assets/vendors/owl-carousel-2/owl.carousel.min.js"></script>
-    <script src="assets/js/jquery.cookie.js" type="text/javascript"></script>
+    <script src="../../assets/vendors/chart.js/Chart.min.js"></script>
+    <script src="../../assets/vendors/progressbar.js/progressbar.min.js"></script>
+    <script src="../../assets/vendors/jvectormap/jquery-jvectormap.min.js"></script>
+    <script src="../../assets/vendors/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+    <script src="../../assets/vendors/owl-carousel-2/owl.carousel.min.js"></script>
+    <script src="../../assets/js/jquery.cookie.js" type="text/javascript"></script>
     <!-- End plugin js for this page -->
     <!-- inject:js -->
-    <script src="assets/js/off-canvas.js"></script>
-    <script src="assets/js/hoverable-collapse.js"></script>
-    <script src="assets/js/misc.js"></script>
-    <script src="assets/js/settings.js"></script>
-    <script src="assets/js/todolist.js"></script>
+    <script src="../../assets/js/off-canvas.js"></script>
+    <script src="../../assets/js/hoverable-collapse.js"></script>
+    <script src="../../assets/js/misc.js"></script>
+    <script src="../../assets/js/settings.js"></script>
+    <script src="../../assets/js/todolist.js"></script>
     <!-- endinject -->
     <!-- Custom js for this page -->
-    <script src="assets/js/dashboard.js"></script>
+    <script src="../../assets/js/dashboard.js"></script>
     <!-- End custom js for this page -->
   </body>
 </html>
